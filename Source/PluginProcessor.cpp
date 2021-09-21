01d58d98e06bc9ea6550aed2ac31912565fdc90c
Francois DECOURCELLE <francois.decourcelle@gmail.com>
2021-09-21 18:51:02 +0200

    part0: oscillator

---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,185 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+JuceOscillatorAudioProcessor::JuceOscillatorAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+}
+
+JuceOscillatorAudioProcessor::~JuceOscillatorAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String JuceOscillatorAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool JuceOscillatorAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool JuceOscillatorAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool JuceOscillatorAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double JuceOscillatorAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int JuceOscillatorAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int JuceOscillatorAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void JuceOscillatorAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String JuceOscillatorAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void JuceOscillatorAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void JuceOscillatorAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    juce::dsp::ProcessSpec spec;
+    spec.sampleRate = sampleRate;
+    spec.maximumBlockSize = samplesPerBlock;
+    spec.numChannels = getTotalNumOutputChannels();
+    
+    osc.prepare(spec);
+    osc.setFrequency(220.0f);
+    
+    gain.prepare(spec);
+    gain.setGainLinear(0.01f);
+}
+
+void JuceOscillatorAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool JuceOscillatorAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void JuceOscillatorAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+    
+    juce::dsp::AudioBlock<float> audioBlock {buffer};
+    osc.process(juce::dsp::ProcessContextReplacing<float> (audioBlock));
+    
+    gain.process(juce::dsp::ProcessContextReplacing<float> (audioBlock));
+}
+
+//==============================================================================
+bool JuceOscillatorAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* JuceOscillatorAudioProcessor::createEditor()
+{
+    return new JuceOscillatorAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void JuceOscillatorAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void JuceOscillatorAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new JuceOscillatorAudioProcessor();
+}
